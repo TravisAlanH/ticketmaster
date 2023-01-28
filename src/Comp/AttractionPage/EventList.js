36645f60cb7cb6b2ad42f1eb3b5571a0ae2bb833
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2023-01-28 17:16:53 -0600

    Other Info Drop Down Started

--- src/Comp/AttractionPage/EventList.js
+++ src/Comp/AttractionPage/EventList.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React from "react";
 import { FaEllipsisV } from "react-icons/fa";
+import OtherAttractionsComp from "./OtherAttractionsComp";
 
 const apiKey = process.env.REACT_APP_TM_API;
 
@@ -17,6 +18,10 @@ export default function EventList({ id }) {
         console.log(error);
       });
   }, [id]);
+
+  function viewDrop(id) {
+    document.getElementById(id).classList.toggle("h-0");
+  }
 
   //// LOAD
   if (eventList.length === 0) return null;
@@ -41,34 +46,44 @@ export default function EventList({ id }) {
           let monDay = monthArray[date.getMonth()] + " " + numberDay;
           let nameDate = dayArray[date.getDay()];
           // location
-          let location = items._embedded.venues[0].name;
+          let location = "test";
+          if ("venues" in items._embedded) location = items._embedded.venues[0].name;
+          // OtherAttractions
+          let OtherAttractions = <div className="hidden"></div>;
+          if ("attractions" in items._embedded) OtherAttractions = <OtherAttractionsComp OtherAttractions={items._embedded.attractions} />;
           console.log(items);
           return (
-            <div className="flex flex-row border" key={index}>
-              <div id="data" className=" w-11/12 h-auto">
-                <div className="flex flex-col lg:flex-row">
-                  <div className="flex flex-row gap-2">
-                    <span className="text-sm w-[3.8rem] text-[#112553f8] font-extrabold">{monDay}</span>
-                    <div className="flex flex-col justify-end">
-                      <span className="text-xs w-[3rem]">{nameDate}</span>{" "}
+            <div className="flex flex-col h-auto">
+              <div className="flex flex-row border" key={index}>
+                <div id="data" className=" w-11/12 h-auto">
+                  <div className="flex flex-col lg:flex-row">
+                    <div className="flex flex-row gap-0">
+                      <span className="text-sm w-[3.8rem] text-[#112553f8] font-extrabold">{monDay}</span>
+                      <div className="flex flex-col justify-end">
+                        <span className="text-xs w-[3rem]">{nameDate}</span>{" "}
+                      </div>
+                    </div>
+                    {/* <div>asdf</div> */}
+                    <div className="flex flex-col lg:flex-row lg:justify-between w-full">
+                      <span className="text-sm lg:hidden">{nameMobile}</span>
+                      <span className="text-sm hidden lg:block">{nameLarge}</span>
+                      <span className="lg:text-sm lg:mr-16 text-xs text-gray-500">{location}</span>
                     </div>
                   </div>
-                  {/* <div>asdf</div> */}
-                  <div className="flex flex-col lg:flex-row lg:justify-between w-full">
-                    <span className="text-sm lg:hidden">{nameMobile}</span>
-                    <span className="text-sm hidden lg:block">{nameLarge}</span>
-                    <span className="lg:text-sm lg:mr-16 text-xs text-gray-500">{location}</span>
+                </div>
+                <div id="moreInfoToggle" className=" w-1/12 h-12 lg:h-auto lg:flex flex-row justify-center items-center hidden">
+                  <div>
+                    <button className=" px-4 rounded-md" onClick={() => viewDrop("drop" + index)}>
+                      info
+                    </button>
                   </div>
                 </div>
-              </div>
-
-              <div id="moreInfoToggle" className=" w-1/12 h-12 lg:flex flex-row justify-center items-center hidden">
-                <div>
-                  <button className=" px-4 rounded-md">info</button>
+                <div id="moreInfoToggle" className=" w-1/12 h-12 flex flex-row justify-center items-center lg:hidden">
+                  <FaEllipsisV className=" p-1 w-5 h-7" onClick={() => viewDrop("drop" + index)} />
                 </div>
               </div>
-              <div id="moreInfoToggle" className=" w-1/12 h-12 flex flex-row justify-center items-center lg:hidden">
-                <FaEllipsisV className=" p-1 w-5 h-7" />
+              <div id={"drop" + index} className="w-full transition-all h-0 overflow-hidden">
+                {OtherAttractions}
               </div>
             </div>
           );
