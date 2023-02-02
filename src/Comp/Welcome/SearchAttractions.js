b66513021123bddcf916aae2eb8447d670efff08
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2023-02-02 11:38:06 -0600

    Remove Last Commit

--- src/Comp/Welcome/SearchAttractions.js
+++ src/Comp/Welcome/SearchAttractions.js
@@ -109,9 +109,9 @@ export default function SearchAttractions({ setSaved, saved, setInputItem, input
   }
 
   return (
-    <div className="w-screen">
+    <div className="">
       <form
-        className="relative inline-block flex flex-row justify-center w-full"
+        className="relative inline-block"
         onSubmit={(event) => {
           event.preventDefault();
           search(document.getElementById("Input").value);
@@ -128,7 +128,7 @@ export default function SearchAttractions({ setSaved, saved, setInputItem, input
             AttractionListBuild(e.target.value);
           }}
         />
-        <div id="DropDown" className="hidden absolute overflow-auto z-10 bg-gray-200 left-0 border border-black p-4 rounded-lg mt-8">
+        <div id="DropDown" className="hidden absolute overflow-auto z-10 bg-gray-200 left-0 border border-black p-4 rounded-lg">
           {AttractionList.map((items, index) => {
             let name = items.name;
             if (items.name.length > 33) name = items.name.match(/[\s\S]{1,30}/g)[0] + "...";
